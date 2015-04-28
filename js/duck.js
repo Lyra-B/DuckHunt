03129d8f74e0c2c9e65ce8df57a6a04f4553c828
Lyra-B <glykeriapeppa@gmail.com>
2015-04-28 12:00:49 +0100

    Added code in die prototype function

--- js/duck.js
+++ js/duck.js
@@ -52,14 +52,14 @@ Duck.prototype.draw = function() {
 // TODO: I've been shot!
   Duck.prototype.die = function() {
   // Add a .dead CSS class
-
+    $(this.el)[0].className = 'dead';
   // Stop flapping - clear the flapTimer
-
+    clearTimeout(this.flapTimer);
   // Stop flying animations
-
+    $(this.el).stop();
   // Notify the Game object and add 100 to the score
-
   // Fall to the bottom of the screen
+    $(this.el).animate({bottom: '+500px'}, 6000);
 };
 
 // I made it to the other side!
